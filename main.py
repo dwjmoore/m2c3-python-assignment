d95314393030dc424ddbf29209fc8b1dcef99954
David Moore <100255970+dwjmoore@users.noreply.github.com>
2022-04-06 20:32:06 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+#Exercise 1:
+print("Exercise 1")
+
+string = "this is a string"
+number = 5
+list = ["apple", "banana", "orange"]
+boolean = 10 > 9
+
+print(string)
+print(number)
+print(list)
+print(boolean)
+
+#Exercise 2
+print("\nExercise 2")
+
+index_string = string[0:3]
+print(index_string)
+
+#Exercise 3
+print("\nExercise 3")
+
+index_list = list[0]
+print(index_list)
+
+#Exercise 4
+print("\nExercise 4")
+
+number_2 = number + 10
+print(number_2)
+
+#Exercise 5
+print("\nExercise 5")
+
+index_list_2 = list[-1]
+print(index_list_2)
+
+#Exercise 6
+print("\nExercise 6")
+
+names = "harry,alex,susie,jared,gail,conner"
+names_list = names.split(',')
+print(names_list)
+
+#Exercise 7
+print("\nExercise 7")
+
+string_2 = string.replace("this", string[0:4].upper())
+print(string_2)
+
+#Exercise 8
+print("\nExercise 8")
+
+sentence = f"Your lucky number is {number}."
+print(sentence)
+
+#Exercise 8
+print("\nExercise 8")
+
+print("hello world")
